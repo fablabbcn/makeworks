6fc9b68ee3f14b0938bb150972f04e447ae73a77
Viktor Smari <viktorsmari@gmail.com>
2020-02-17 09:47:49 +0100

    Simple form btn class: btn btn-primary

--- config/application.rb
+++ config/application.rb
@@ -15,5 +15,10 @@ module Makeworks
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+
+    config.generators do |g|
+      g.stylesheets false
+      g.helper false
+    end
   end
 end
