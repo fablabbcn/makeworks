614998223d877bd8577f4f15d9e479195e9faebc
Viktor Smari <viktorsmari@gmail.com>
2020-02-23 22:25:01 +0100

    Show all regions on frontpage

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,5 +1,6 @@
 class WelcomeController < ApplicationController
   def index
+    @all_regions = Region.all
     @random_companies = Company.all.sample(5)
   end
 end
--- app/models/region.rb
+++ app/models/region.rb
@@ -1,4 +1,5 @@
 class Region < ApplicationRecord
   has_many :user_regions
   has_many :users, through: :user_regions
+  has_many :companies
 end
