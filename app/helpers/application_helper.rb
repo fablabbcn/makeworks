dd90ff299b2ff18507b55eb32b046baef038bc19
Viktor Smari <viktorsmari@gmail.com>
2020-03-27 15:23:50 +0100

    Add translations + flash helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,25 @@
 module ApplicationHelper
+  def bootstrap_class_for(flash_type)
+    {
+      success: "alert-success",
+      error: "alert-danger",
+      alert: "alert-warning",
+      notice: "alert-info"
+    }.stringify_keys[flash_type.to_s] || flash_type.to_s
+  end
+
+  def flash_messages(opts = {})
+    flash.each do |msg_type, message|
+      concat(content_tag(:div, message,
+                         class: "alert #{bootstrap_class_for(msg_type)} position-fixed shadow px-4 w-25 mt-3",
+                         role: "alert",
+                         style: 'z-index:200; right:5%; opacity: 0.8;'
+                        ) do
+        concat content_tag(:button, 'x', class: "close", data: { dismiss: 'alert' })
+        concat message
+      end)
+    end
+    nil
+  end
+
 end
