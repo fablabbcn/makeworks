48859539f9e00242faf9449bbdcb1e7bb52499d2
Viktor Smari <viktorsmari@gmail.com>
2020-04-14 17:03:52 +0200

    Only admins can edit blog

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,6 +1,15 @@
 class BlogsController < ApplicationController
   before_action :set_blog, only: [:show, :edit, :update, :destroy]
   skip_before_action :authenticate_user!, only: [:show, :index]
+  before_action :who_can_edit!, only: [:edit, :destroy, :update]
+
+  def who_can_edit!
+    if current_user.is_admin?
+    else
+      flash[:error] = 'Only admins can edit'
+      redirect_to @blog
+    end
+  end
 
   # GET /blogs
   # GET /blogs.json
