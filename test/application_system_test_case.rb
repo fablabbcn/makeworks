933efffe18a913e8b989b7e1c384ee065ee1d4d0
Viktor Smari <viktorsmari@gmail.com>
2020-04-28 12:24:12 +0200

    Add system tests 'hack' for local testing

--- test/application_system_test_case.rb
+++ test/application_system_test_case.rb
@@ -1,6 +1,14 @@
 require "test_helper"
 
-Webdrivers::Chromedriver.required_version = '83.0.4103.14'
+# System tests are failing locally with this error:
+# Webdrivers::NetworkError: Net::HTTPServerException: 404 "Not Found" with
+# https://chromedriver.storage.googleapis.com/LATEST_RELEASE_84.0.4115
+
+# Adding this 'hack' meanwhile
+if ENV['CUSTOM_CHROME_VERSION']
+  Webdrivers::Chromedriver.required_version = '83.0.4103.14'
+end
+
 class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
   if ENV['SYSTEM_TEST']
     driven_by :selenium, using: ENV['SYSTEM_TEST'].to_sym, screen_size: [1400, 1400]
