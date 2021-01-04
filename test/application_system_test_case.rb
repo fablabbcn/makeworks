39e65c68072e87f9a666afd74472a9fd77f9d0fe
Viktor Smari <viktorsmari@gmail.com>
2021-01-04 08:43:38 +0100

    Remove unused method / hack

--- test/application_system_test_case.rb
+++ test/application_system_test_case.rb
@@ -1,14 +1,5 @@
 require "test_helper"
 
-# System tests are failing locally with this error:
-# Webdrivers::NetworkError: Net::HTTPServerException: 404 "Not Found" with
-# https://chromedriver.storage.googleapis.com/LATEST_RELEASE_84.0.4115
-
-# Adding this 'hack' meanwhile
-if ENV['CUSTOM_CHROME_VERSION']
-  Webdrivers::Chromedriver.required_version = '83.0.4103.14'
-end
-
 class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
   include CapybaraSelect2
 
