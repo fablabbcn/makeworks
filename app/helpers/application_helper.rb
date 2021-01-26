5cb0da6671f2245f91f3f92d3a130fd8ccfd548b
Viktor Smari <viktorsmari@gmail.com>
2021-01-26 18:35:26 +0100

    Use html_safe on title to see ' characters

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -29,7 +29,7 @@ module ApplicationHelper
   end
 
   def title(page_title, options = {})
-    content_for(:title, page_title.to_s)
+    content_for(:title, page_title.to_s.html_safe)
     content_tag(:h1, page_title, options.merge(itemprop: 'name'))
   end
 
