c55e6898c979eccb1c9fe5418db89ca8625fae5e
Viktor Smari <viktorsmari@gmail.com>
2021-02-02 21:17:48 +0100

    Add Trackable, sign in IP / date info

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,9 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable,
+         :trackable,
          :omniauthable, omniauth_providers: [:fablabs, :linkedin]
   has_many :user_regions, dependent: :destroy
   has_many :regions, through: :user_regions
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_28_183921) do
+ActiveRecord::Schema.define(version: 2021_02_02_195944) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -319,6 +319,11 @@ ActiveRecord::Schema.define(version: 2021_01_28_183921) do
     t.string "slug"
     t.string "locale"
     t.string "time_zone", default: "UTC"
+    t.integer "sign_in_count", default: 0, null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.inet "current_sign_in_ip"
+    t.inet "last_sign_in_ip"
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
     t.index ["slug"], name: "index_users_on_slug", unique: true
   end
