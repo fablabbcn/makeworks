e7bf19c71018e46286a9480b4ba50e9392f265e1
Viktor Smari <viktorsmari@gmail.com>
2021-04-21 17:36:05 +0200

    Bootstrap icons: Try using web fonts

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,6 +8,7 @@ require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
 require("chartkick/chart.js")
+require("bootstrap-icons/font/bootstrap-icons")
 
 
 // Uncomment to copy all static images under ../images to the output folder and reference
