57968f755cfc0b7e2cd0feed2d0d39d6fe6efc58
Viktor Smari <viktorsmari@gmail.com>
2021-07-01 12:24:02 +0200

    Use new.make.works for emails default url

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -118,7 +118,7 @@ Rails.application.configure do
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
 
-  config.action_mailer.default_url_options = { host: 'make.works' }
+  config.action_mailer.default_url_options = { host: 'new.make.works' }
   config.action_mailer.delivery_method = :mailgun
   config.action_mailer.mailgun_settings = {
     api_key: ENV['MAILGUN_API_KEY'],
