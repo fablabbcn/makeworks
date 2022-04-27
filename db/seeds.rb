a87f7448dfc0f7eaf9b5e8a29ff0191f2df666c8
benyscott <beny.scott@gmail.com>
2022-04-27 13:09:21 +0200

    Created seed file to fill database if Make.Works is run locally

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,9 +5,65 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-User.create!(
-  email: 'admin@example.com',
-  password: 'password',
-  password_confirmation: 'password',
-  is_admin: true
-)
+
+20.times do |x|
+  User.create!(
+    email: Faker::Internet.email,
+    password: Faker::Internet.password,
+    is_admin: false
+  )
+end
+
+15.times do |x|
+  Region.create!(
+    name: Faker::Address.city,
+    lat: Faker::Address.latitude;
+    lng: Faker::Address.longitude
+  )
+end
+
+15.times do |x|
+  Company.create!(
+    name: Faker::Company.name,
+    address: Faker::Address.street_address
+    lat: Faker::Address.latitude;
+    lng: Faker::Address.longitude
+  )
+end
+
+10.times do |x|
+  IndustryTaxonomy.create!(
+    name: Faker::Company.industry
+  )
+end
+
+10.times do |x|
+  Material.create!(
+    name: Faker::Construction.material
+  )
+end
+
+10.times do |x|
+  MachinesTaxonomy.create!(
+    name: Faker::Appliance.equipment
+  )
+end
+
+10.times do |x|
+  ProcessTaxonomy.create!(
+    name: Faker::Verb.ing_form
+  )
+end
+
+10.times do |x|
+  FinishedProductsTaxonomy.create!([
+    { name: "Ancient techniques" },
+    { name: "Family business" },
+    { name: "Innovative production" },
+    { name: "Family business" },
+    { name: "One of a kind (bespoke)" },
+    { name: "Vegan" },
+    { name: "Woman owned-business" },
+    { name: "Zero waste" }
+  ])
+end
