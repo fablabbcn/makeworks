8dc72fae4f910c11b47bcc1ff487e017f35cf1fd
Tim Cowlishaw <tim@timcowlishaw.co.uk>
2023-12-01 16:32:41 +0100

    few other tweaks for easier local development

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -7,6 +7,7 @@ require "active_support/core_ext/integer/time"
 
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
+  config.assets.compile = true
 
   config.cache_classes = false
   config.action_view.cache_template_loading = true
