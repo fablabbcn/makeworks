71cf8c7000726a3b24718af6c857f14e7b8de08b
Tim Cowlishaw <tim@timcowlishaw.co.uk>
2024-01-10 20:50:01 +0100

    add discard to user model for soft deletions

--- Gemfile
+++ Gemfile
@@ -15,6 +15,7 @@ gem 'country_select'
 gem "devise", "~> 4.7"
 gem "devise_invitable", "~> 2"
 gem "devise-i18n"
+gem 'discard', '~> 1.2'
 gem 'friendly_id', '~> 5.3.0' # 5.4.0 has a breaking change! https://github.com/norman/friendly_id/pull/787
 gem 'geocoder'
 gem 'groupdate'
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,9 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
+  include Discard::Model
+  default_scope { kept }
+
   devise :database_authenticatable, :registerable,
          :invitable, :confirmable,
          :recoverable, :rememberable, :validatable,
